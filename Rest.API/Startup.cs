46ceef8e04d1e9be21f832d45c0b827588dff6db
Diego <diego.torres@fitideas.co>
2020-03-18 20:15:11 +1100

    change lower option

--- Rest.API/Startup.cs
+++ Rest.API/Startup.cs
@@ -32,7 +32,7 @@ namespace Rest.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRouting(options => options.LowercaseUrls = true);
+            //services.AddRouting(options => options.LowercaseUrls = true);
             services
                 .AddMvc(options =>
                 {
